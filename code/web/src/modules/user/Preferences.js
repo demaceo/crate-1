b2e6750253d546350a2b96cefcd12e7dbaf5cda4
Peyton Jones <peytonj.dev@gmail.com>
2021-02-08 11:11:38 -0700

    Styling added for survey questions

--- code/web/src/modules/user/Preferences.js
+++ code/web/src/modules/user/Preferences.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
+import ImageTile from '../../ui/image/Tile'
 
 // UI Imports
 import { Grid, GridCell } from "../../ui/grid";
@@ -17,43 +18,48 @@ class Preferences extends PureComponent {
   }
 
   render() {
-    return
+    return (
       <GridCell>
         <Helmet>
           <title> Choose Your Style Survey - Crate</title>
         </Helmet>
-        <Grid gutter={true} alignCenter={true}>
-          <p>Please Choose a Shirt</p>
-          <p>Which Bottoms would you wear on a night out?</p>
-          <p>Which shoes would you wear on a dialy basis</p>
-          <p>Which hat would you wear under the hot sun?</p>
+        <Grid gutter={true} alignCenter={true}
+          style={{display:'flex',
+            flex: 1,
+            flexDirection:'column',
+            marginTop:'15vh'}}
+            >
+          <p style={{padding: "2%"}}>Please Choose a Shirt</p>
+          <p style={{padding: "2%"}}>Which Bottoms would you wear on a night out?</p>
+          <p style={{padding: "2%"}}>Which shoes would you wear on a dialy basis</p>
+          <p style={{padding: "2%"}}>Which hat would you wear under the hot sun?</p>
           <GridCell justifyCenter={true}>
-            <ImageTile width={300} height={530} shadow={level1} />
+            <ImageTile width={300} height={530} />
           </GridCell>
           <GridCell>
             <Grid>
               <GridCell justifyCenter={true}>
-                <ImageTile width={170} height={250} shadow={level1}/>
+                <ImageTile width={170} height={250} />
               </GridCell>
             </Grid>
             <Grid>
               <GridCell justifyCenter={true}>
-                <ImageTile width={170} height={250} shadow={level1} image={`${APP_URL}/images/stock/women/3.jpg`}
-                  style={{ marginTop: '1.9em' }} />
+                <ImageTile width={170} height={250} />
               </GridCell>
             </Grid>
           </GridCell>
         </Grid>
       </GridCell>
+    )
   }
 }
 
 // Must retrieve style photos from backend (maybe a fetch request) or
-    // directly from store?
-    // will need to map over those photos and put them into Tile components?
-    // We would like 5 rows of images, (3 images per row)
-    // Images will be selectable
-    // There will be a "submit survey" button (this will change the route to /subscriptions)
-    // An array of ids will be sent to update the global state (store)
+// directly from store?
+// will need to map over those photos and put them into Tile components?
+// We would like 5 rows of images, (3 images per row)
+// Images will be selectable
+// There will be a "submit survey" button (this will change the route to /subscriptions)
+// An array of ids will be sent to update the global state (store)
 
 export default Preferences;
