b5faed178591bf99d9769631a6238a790adb2f5b
Peyton Jones <peytonj.dev@gmail.com>
2021-02-08 09:13:53 -0700

    add boiler plate outline to preferences

--- code/web/src/modules/user/Preferences.js
+++ code/web/src/modules/user/Preferences.js
@@ -17,15 +17,39 @@ class Preferences extends PureComponent {
   }
 
   render() {
-    // Must retrieve style photos from backend (maybe a fetch request) or
+    return
+      <GridCell>
+        <Helmet>
+          <title> Choose Your Style Survey - Crate</title>
+        </Helmet>
+        <Grid gutter={true} alignCenter={true}>
+          <GridCell justifyCenter={true}>
+            <ImageTile width={300} height={530} shadow={level1} />
+          </GridCell>
+          <GridCell>
+            <Grid>
+              <GridCell justifyCenter={true}>
+                <ImageTile width={170} height={250} shadow={level1}/>
+              </GridCell>
+            </Grid>
+            <Grid>
+              <GridCell justifyCenter={true}>
+                <ImageTile width={170} height={250} shadow={level1} image={`${APP_URL}/images/stock/women/3.jpg`}
+                  style={{ marginTop: '1.9em' }} />
+              </GridCell>
+            </Grid>
+          </GridCell>
+        </Grid>
+      </GridCell>
+  }
+}
+
+// Must retrieve style photos from backend (maybe a fetch request) or
     // directly from store?
     // will need to map over those photos and put them into Tile components?
     // We would like 5 rows of images, (3 images per row)
     // Images will be selectable
     // There will be a "submit survey" button (this will change the route to /subscriptions)
     // An array of ids will be sent to update the global state (store)
-    return <div>Hello friends</div>;
-  }
-}
 
 export default Preferences;
