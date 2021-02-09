4093716796409972b36d600b8253cc176f9e5d32
Sean Steel <seanrsteel@gmail.com>
2021-02-09 15:54:26 -0700

    Setup test for the user mutation createStylePreference; test is failing
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Sheryl Stillman <7945439+stillsheryl@users.noreply.github.com>

---
+++ code/api/src/modules/user/test/mutation.test.js
@@ -0,0 +1,42 @@
+import request from 'supertest';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from '../../../setup/schema/index';
+import connection from '../../../setup/database'
+
+describe('Test user mutations', () => {
+  let server;
+    beforeAll(() => {
+       server = express();
+       server.use(
+           "/",
+           graphqlHTTP({
+               schema: schema,
+               graphiql: false
+           })
+       )
+    })
+  it('update a users style type', async (done) => {
+      const response = await request(server)
+        .post('/graphql')
+        .send({query: 'mutation { createStylePreference(id:2, survey: true, styleArray: ["goth", "athletic", "beach", "athletic"]) {id survey stylePreference} }'})
+        .expect(200)
+        expect(response.body).toMatchObject(
+          {
+            data: {
+              createStylePreference: {
+                id: 2,
+                stylePreference: "athletic"
+                }
+              }
+            }
+          )
+
+    done();
+  });
+
+  afterAll(done => {
+    connection.close();
+    done();
+  });
+});
